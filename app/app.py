59ba6165cf58b4ffa805c066f4d3d1bb374460f9
ahilajebin <ahilajeb@gmail.com>
2026-02-15 15:56:12 +0000

    Added port 8080 setup to app.py

--- app/app.py
+++ app/app.py
@@ -18,4 +18,4 @@ def index():
     return render_template("index.html", answer=answer)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
